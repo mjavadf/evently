c9ca52b30cd8ce9628bbad9d0f85335367fa2b8c
mjavadf <javad.me1376@gmail.com>
2023-06-14 15:59:09 +0200

    change secter key :)

--- evently/settings.py
+++ evently/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=vsjun5f9#$m1%8jc*5cjhf5sopjk1sg7lj8d*p#^uet#i&8zl'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
