b5a35c8346583a572246a663fd74fee5b2eb1613
mjavadf <javad.me1376@gmail.com>
2023-06-14 15:46:26 +0200

    implement events models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    capacity = models.IntegerField()
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
+    slug = models.SlugField(max_length=255, unique=True)
+    
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    events = models.ManyToManyField(Event, related_name='categories')
+    slug = models.SlugField(max_length=255, unique=True)
