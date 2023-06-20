04c60c493b7fdd8ab016f0f1f6e5b0711de4dcc0
mjavadf <javad.me1376@gmail.com>
2023-06-20 12:49:03 +0200

    refactor events model

--- events/models.py
+++ events/models.py
@@ -1,5 +1,17 @@
+from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 class Event(models.Model):
     title = models.CharField(max_length=255)
@@ -7,10 +19,9 @@ class Event(models.Model):
     date = models.DateTimeField()
     location = models.CharField(max_length=255)
     capacity = models.IntegerField()
-    organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='organizing_events')
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
     slug = models.SlugField(max_length=255, unique=True)
     
-class Category(models.Model):
-    name = models.CharField(max_length=255)
-    events = models.ManyToManyField(Event, related_name='categories')
-    slug = models.SlugField(max_length=255, unique=True)
+    def __str__(self):
+        return f'{self.title} by {self.organizer}'
