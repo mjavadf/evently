461d85d99e3a5a4814d675955fe19395c378fd25
mjavadf <javad.me1376@gmail.com>
2023-06-20 18:32:55 +0200

    create ticket model

--- events/models.py
+++ events/models.py
@@ -25,3 +25,24 @@ class Event(models.Model):
     
     def __str__(self):
         return f'{self.title} by {self.organizer}'
+
+
+class Ticket(models.Model):
+    event = models.ForeignKey('Event', on_delete=models.CASCADE, related_name='tickets')
+    title = models.CharField(max_length=255)
+    descriptin = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    capacity = models.IntegerField()
+    purchased = models.IntegerField(default=0)
+    available = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f'{self.title} for {self.event}'
+    
+    def buy(self, quantity=1):
+        if self.available:
+            self.sold += quantity
+            self.available = self.sold < self.capacity
+            self.save()
+            return True
+        return False
