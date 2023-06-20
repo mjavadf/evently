5ab4076a1dc71fab6f72d3327f413d838d579d96
mjavadf <javad.me1376@gmail.com>
2023-06-20 18:42:14 +0200

    install rest_framework

--- evently/settings.py
+++ evently/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.sessions",
     "django.contrib.staticfiles",
+    'rest_framework',
     "events.apps.EventsConfig",
     "registration.apps.RegistrationConfig",
 ]
