2d0a9ea9833e28b4ef67da3964aa9c05fdcd755f
mjavadf <javad.me1376@gmail.com>
2023-06-20 12:49:46 +0200

    implement admin models

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("title", "date", "location", "organizer", "category")
+    list_filter = ("organizer", "date")
+    fields = (
+        "title",
+        "slug",
+        "description",
+        "date",
+        "location",
+        "capacity",
+        "organizer",
+        "category",
+    )
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields = ("title", "category__name", "location")
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "event_count")
+    prepopulated_fields = {"slug": ("name",)}
+
+    def event_count(self, category):
+        url = (
+            reverse("admin:events_event_changelist")
+            + f"?category__id__exact={category.id}"
+        )
+        return format_html('<a href="{}">{}</a>', url, category.event_set.count())
--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Registration)
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ("event", "participant", "status")
+    list_filter = ["status", 'payment_status']
+    search_fields = ("event__title", "user__username")
+    fields = [
+        "participant",
+        "event",
+        "status",
+        "payment_status",
+        "payment_amount",
+        "payment_method",
+    ]
+    autocomplete_fields = ["participant", "event"]
