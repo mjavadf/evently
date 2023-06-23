1403037b30874a0f670d705e55985b2511ad6dfa
mjavadf <javad.me1376@gmail.com>
2023-06-23 22:23:00 +0200

    Use ModelViewSet mixin for products api

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
 
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('event/<int:id>/', views.event_detail, name='event_detail'),
-]
+router = DefaultRouter()
+router.register("events", views.EventViewSet)
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('', views.home, name='home'),
+#     path('event/<int:id>/', views.event_detail, name='event_detail'),
+# ]
--- events/views.py
+++ events/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from events import serializers
 from events.serializers import EventSerializer, EventListSerializer
 from .models import Event
 
@@ -11,8 +13,18 @@ def home(request):
     serializer = EventListSerializer(events, many=True)
     return Response(serializer.data)
 
+
 @api_view(["GET"])
 def event_detail(request, id):
     event = get_object_or_404(Event, id=id)
     serializer = EventSerializer(event)
     return Response(serializer.data)
+
+
+class EventViewSet(ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_classes = {"list": EventListSerializer}
+    default_serializer_class = EventSerializer
+    
+    def get_serializer_class(self):
+        return self.serializer_classes.get(self.action, self.default_serializer_class)
