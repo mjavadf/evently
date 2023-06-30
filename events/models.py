74f8c5a4eda4a7062225cf84216770be42ae9cc6
mjavadf <javad.me1376@gmail.com>
2023-06-30 18:27:19 +0200

    Move registration model to events app

--- events/models.py
+++ events/models.py
@@ -44,3 +44,34 @@ class Ticket(models.Model):
             self.save()
             return True
         return False
+    
+    
+class Registration(models.Model):
+    STATUS_CHOICES = [
+        ('P', 'Pending'),
+        ('A', 'Approved'),
+        ('R', 'Rejected'),
+        ('W', 'Waitlisted'),
+    ]
+    
+    PAYMENT_STATUS_CHOICES = [
+        ('P', 'Pending'),
+        ('C', 'Complete'),
+        ('F', 'Failed'),
+        ('N', 'Not Required')
+    ]
+    
+    event = models.ForeignKey(Event, on_delete=models.PROTECT)
+    participant = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    registration_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=1, 
+                              choices=STATUS_CHOICES, 
+                              default='P')
+    payment_status = models.CharField(max_length=1,
+                                      choices=PAYMENT_STATUS_CHOICES, 
+                                      default='N')
+    payment_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    payment_method = models.CharField(max_length=50, default='Not Required')
+    
+    def __str__(self):
+        return f"{self.participant.username} - {self.event.title} Registration"
