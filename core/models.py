8f0b01c3e316809e831a697fbe997e555a6e6810
mjavadf <javad.me1376@gmail.com>
2023-06-30 21:58:43 +0200

    remove phone_number from user's model

--- core/models.py
+++ core/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=20, unique=True, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
     
