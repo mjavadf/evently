9dee5f91e39976c37161263c8940e4be1b3721ce
Javad Farokhi <javad.me1376@gmail.com>
2024-08-17 18:55:40 +0200

    Change db url

--- evently/settings/production.py
+++ evently/settings/production.py
@@ -11,7 +11,7 @@ ALLOWED_HOSTS = ["*"]
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://evently_idl4_user:YTZXovOcPTaBbywbYPqJfyjVofW1KJo3@dpg-cr0d2ujv2p9s73a5u06g-a/evently_idl4',
+        default='postgresql://evently_xpje_user:lPdeOdzNpPPjxYm6TI6AkhQa3K8Q3Hs8@dpg-cr0d86bv2p9s73a6190g-a/evently_xpje',
         conn_max_age=600
     )
 }
